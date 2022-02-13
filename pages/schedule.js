4aa830626b4e39a963f56fd4336bc44e80f4d14b
Jake McGuire <jakemcguire2@gmail.com>
2022-02-13 15:50:36 -0700

    Schedule changes

--- pages/schedule.js
+++ pages/schedule.js
@@ -15,7 +15,7 @@ export default function Schedule() {
       <div className={styles.schedule}>
         <span className={styles.day}>Friday, April 8</span>
         <span className={styles.eventDetails}>
-            <span>Welcome Drinks</span>
+            <span>Welcome Drinks, Dinner, and Karaoke</span>
             <span><a href="https://www.thereefpalmsprings.com/" target="_blank" rel="noreferrer">The Reef</a> (411 E. Palm Canyon Drive)</span>
             <span>6:00-10:00 PM</span>
         </span>
@@ -23,22 +23,17 @@ export default function Schedule() {
         <span className={styles.eventDetails}>
             <span>Wedding Ceremony</span>
             <span>The Commune Garden @ The Ace Hotel</span>
-            <span>5:00-5:30 PM</span>
+            <span>4:30-5:00 PM</span>
         </span>
         <span className={styles.eventDetails}>
             <span>Cocktail Hour (featuring <a href="https://www.theblanktapes.com/" target="_blank" rel="noreferrer">The Blank Tapes</a>)</span>
             <span>The Commune @ The Ace Hotel</span>
-            <span>5:30-6:30 PM</span>
+            <span>5:00-6:30 PM</span>
         </span>
         <span className={styles.eventDetails}>
             <span>Dinner and Dancing</span>
             <span>The Commune @ The Ace Hotel</span>
             <span>6:30-11:00 PM</span>
-        </span>
-        <span className={styles.eventDetails}>
-            <span>After Party</span>
-            <span>The Amigo Room @ The Ace Hotel</span>
-            <span>11:00 PM - Late</span>
         </span>
       </div>
     </section>
