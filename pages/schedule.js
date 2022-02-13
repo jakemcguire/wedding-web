0275aff60a8c6235ae57de577575506ae747228e
Jake McGuire <jakemcguire2@gmail.com>
2022-02-13 15:55:52 -0700

    Escape characters

--- pages/schedule.js
+++ pages/schedule.js
@@ -33,7 +33,7 @@ export default function Schedule() {
         <span className={styles.eventDetails}>
             <span>Dinner and Dancing</span>
             <span>The Commune @ The Ace Hotel</span>
-            <span>6:30 PM 'til Late</span>
+            <span>6:30 PM &apos;til Late</span>
         </span>
       </div>
     </section>
