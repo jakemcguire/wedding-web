3a7cf126ee9fae6056a52fb54c9e899a5b217637
Jake McGuire <jakemcguire2@gmail.com>
2022-04-08 14:44:16 -0700

    Update home page with key links for wedding weekend

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import splashImage from '../public/images/palm-canyon-drive.webp'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+import scheduleSvg from '../public/images/schedule.svg'
+import videoSvg from '../public/images/video.svg'
+import registrySvg from '../public/images/registry.svg'
 
 export default function Home() {
   return (
     <section>
-      <Image
-        priority
-        src={splashImage}
-        alt="Rebecca Lewis &amp; Jake McGuire are getting married in Palm Springs, California on April 9, 2022"
-      />
+      <h1>Welcome to Palm Springs!</h1>
+      <div className={styles.container}>
+        <div className={styles.linkContainer}>
+          <Image src={scheduleSvg} width={30} height={30} />
+          <Link href="/schedule">
+            <a>Schedule</a>
+          </Link>
+        </div>
+        <div className={styles.linkContainer}>
+          <Image src={videoSvg} width={30} height={30} />
+          <a href="https://www.dropbox.com/request/KBlKRXfwbb9N7vbzOirx" target="_blank" rel="noreferrer" className={styles.link}>Upload Your Toast</a>
+        </div>
+        <div className={styles.linkContainer}>
+          <Image src={registrySvg} width={30} height={30} />
+          <a href="https://www.zola.com/registry/beccajakewedding" target="_blank" rel="noreferrer" className={styles.link}>Registry</a>
+        </div>
+      </div>
     </section>
   )
 }
